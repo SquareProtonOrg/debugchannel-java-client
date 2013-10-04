0d9d29e21e05843fefdde9c7c40989f98c0b8321
Joseph Siddall <joseph@omlet.co.uk>
2013-10-04 08:47:08 +0100

    added src directory and Main class

---
+++ src/uberdebug/client/Main.java
@@ -0,0 +1,18 @@
+package uberdebug.client;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: joseph
+ * Date: 04/10/2013
+ * Time: 08:30
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+
+}
